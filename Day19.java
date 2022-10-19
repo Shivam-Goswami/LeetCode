543281ca7e9d0e4051a35b32bbf109912387bd37
Shivam Goswami <svgoswami.goswami59@gmail.com>
2022-10-19 15:39:50 +0530

    day19

---
+++ Day19.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Day19 {
+    public List<String> topKFrequent(String[] words, int k) {
+        TreeMap<String, Integer> map = new TreeMap<>();
+        for (String a : words) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        ArrayList<String> list = new ArrayList<>(map.keySet());
+        list.sort((x, y) -> map.get(y) - map.get(x));
+        List sub = list.subList(0, k);
+        return sub;
+    }
+}
